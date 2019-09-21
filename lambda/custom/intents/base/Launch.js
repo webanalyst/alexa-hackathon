d4eeb388583cc62ebb66f3774ca6859c7926dc20
Edu Iglesias <eduiglesias@MacBook-Pro-de-Edu.local>
2019-09-21 18:32:07 +0200

    include api

--- lambda/custom/intents/base/Launch.js
+++ lambda/custom/intents/base/Launch.js
@@ -3,24 +3,22 @@ const movie = require('../../api/tmdbApi');
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
     handle(handlerInput) {
-      const {attributesManager} = handlerInput;
-      const requestAttributes = attributesManager.getRequestAttributes();
-      const sessionAttributes = attributesManager.getSessionAttributes();
-      const speechText = requestAttributes.t('WELCOME_MESSAGE');
-      movie().then( res => {
-          sessionAttributes['movie'] = res;
-          console.log('........................');
-          console.log(sessionAttributes['movie'] )
+        const {attributesManager} = handlerInput;
+        const requestAttributes = attributesManager.getRequestAttributes();
+        const sessionAttributes = attributesManager.getSessionAttributes();
+        const speechText = requestAttributes.t('WELCOME_MESSAGE');
+        movie().then(res => {
+            sessionAttributes['movie'] = res;
+            console.log('........................');
+            console.log(sessionAttributes['movie'])
         });
-
-
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .reprompt(speechText)
-        .getResponse();
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .getResponse();
     },
 };
 

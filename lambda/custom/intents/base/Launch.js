cc57a23c76612eef0211a8a315409876ee5be16e
rerades <rerades@siete3.com>
2019-09-21 15:13:56 +0200

    wip

--- lambda/custom/intents/base/Launch.js
+++ lambda/custom/intents/base/Launch.js
@@ -1,11 +1,19 @@
+const movie = require('../../api/tmdbApi');
+
+
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
       return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
     handle(handlerInput) {
-      const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+      const {attributesManager} = handlerInput.attributesManager;
+      const requestAttributes = attributesManager.getRequestAttributes();
+      const sessionAttributes = attributesManager.getSessionAttributes();
       const speechText = requestAttributes.t('WELCOME_MESSAGE');
-  
+
+      console.log(movie)
+      sessionAttributes['movie'] = '' //respuesta;
+
       return handlerInput.responseBuilder
         .speak(speechText)
         .reprompt(speechText)
